6c2bc86b0706d82648ff8033d07bca209d823f97
menichini <paulomenichini@gmail.com>
2024-07-09 18:46:40 -0300

    pipeline: Conversão aerodromos e estruturação dos arquivos Input e Output

--- dags/tarifas_areas_anac.py
+++ dags/tarifas_areas_anac.py
@@ -66,6 +66,37 @@ def tarifas_areas_anac_dag():
         """
             Converte os códigos OACI e faz a carga dos dados
         """
+    # Definir os caminhos dos arquivos
+    public_airfields_path = "data/metadata/cadastro-de-aerodromos-civis-publicos.csv"
+    private_airfields_path = "data/metadata/AerodromosPrivados.csv"
+    output_dir = "TarifasAereas/data/Output"
+
+    # Carregar os arquivos CSV com a codificação 'latin1' e delimitador ';'
+    public_airfields_df = pd.read_csv(public_airfields_path, encoding='latin1', delimiter=';')
+    private_airfields_df = pd.read_csv(private_airfields_path, encoding='latin1', delimiter=';')
+
+    # Selecionar os campos necessários
+    fields = ['Código OACI', 'CIAD', 'Nome', 'Município', 'UF']
+    public_airfields_selected = public_airfields_df[fields]
+    private_airfields_selected = private_airfields_df[fields]
+
+    # Excluir a primeira linha
+    public_airfields_selected = public_airfields_selected.iloc[1:]
+    private_airfields_selected = private_airfields_selected.iloc[1:]
+
+    # Concatenar os dois DataFrames
+    all_airfields_selected = pd.concat([public_airfields_selected, private_airfields_selected])
+
+    # Salvar os dados extraídos em formato JSON e XLS
+    output_json_path = f"{output_dir}/conversao_oaci.json"
+    output_xls_path = f"{output_dir}/conversao_oaci.xls"
+
+    all_airfields_selected.to_json(output_json_path, orient='records', lines=True)
+    all_airfields_selected.to_excel(output_xls_path, index=False)
+
+    # Visualização dos 10 primeiros registros para validação
+    print(all_airfields_selected.head(10))
+
         print(dados_tarifas)
         return dados_tarifas
 
